d764fe19c7ef1774017f824cee8690d152bf73d6
akshay-sri123 <akshay.srivastava.95@gmail.com>
2018-09-11 16:09:57 +0530

    Buffered channels

--- channels/buffered.go
+++ channels/buffered.go
@@ -2,43 +2,27 @@ package main
 
 import (
 	"fmt"
-	// "time"
 	"sync"
+	"time"
 )
 
-// func generateNumbers(buffChan chan int, wg *sync.WaitGroup){
-//   for i := 0; i < 100 ; i++{
-//     buffChan<-i
-//     time.Sleep(100 * time.Millisecond)
-//   }
-//   close(buffChan)
-//   wg.Done()
-// }
-
-func forLoop(wg *sync.WaitGroup) {
+func generateNumbers(buffChan chan int, wg *sync.WaitGroup) {
 	for i := 0; i < 100; i++ {
-		fmt.Printf("%d ", i)
+		buffChan <- i
+		fmt.Printf("%d ", <-buffChan)
+		time.Sleep(100 * time.Millisecond)
 	}
+	close(buffChan)
 	wg.Done()
 }
 
 func main() {
 	var wg sync.WaitGroup
-	// buffChannel := make(chan int, 100)
+	buffChannel := make(chan int, 100)
 
 	wg.Add(1)
 	fmt.Println("Start of the concurrent run.")
-	go forLoop(&wg)
+	go generateNumbers(buffChannel, &wg)
 	wg.Wait()
 	fmt.Println("End of the concurrent run.")
-
-	// fmt.Println("Start of the buffered channel.")
-	// wg.Add(1)
-	// go generateNumbers(buffChannel, &wg)
-	// // fmt.Print(<-buffChannel)
-	// for elem := range buffChannel {
-	//       fmt.Printf("%d ", elem)
-	// }
-	// wg.Wait()
-	// fmt.Println("End of the buffered channel.")
 }
